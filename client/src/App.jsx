1cb2acf2efecf27ae73f1786333dadbbf7e63d4e
dhruv20047228 <dhruvp04004@gmail.com>
2025-12-17 06:16:33 +0530

    frontned features added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import WriteArticle from './pages/WriteArticle'
-import BlogTitles from './pages/BlogTitles'
-import Layout from './pages/Layout'
-import GenerateImage from './pages/GenerateImage'
-import RemoveBackground from './pages/RemoveBackground'
-import RemoveObject from './pages/RemoveObject'
-import ReviewResume from './pages/ReviewResume'
-import Community from './pages/Community'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import WriteArticle from "./pages/WriteArticle";
+import BlogTitles from "./pages/BlogTitles";
+import Layout from "./pages/Layout";
+import GenerateImage from "./pages/GenerateImage";
+import RemoveBackground from "./pages/RemoveBackground";
+import RemoveObject from "./pages/RemoveObject";
+import ReviewResume from "./pages/ReviewResume";
+import Community from "./pages/Community";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/ai' element={<Layout />}>
-        <Route index element={<Dashboard/>}/>
-        <Route path='write-article' element={<WriteArticle/>}/>
-        <Route path='blog-titles' element={<BlogTitles/>}/>
-        <Route path='generate-image' element={<GenerateImage/>}/>
-        <Route path='remove-background' element={<RemoveBackground/>}/>
-        <Route path='remove-object' element={<RemoveObject/>}/>
-        <Route path='review-resume' element={<ReviewResume/>}/>
-        <Route path='community' element={<Community/>}/>
+      <Route path="/ai" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="write-article" element={<WriteArticle />} />
+        <Route path="blog-titles" element={<BlogTitles />} />
+        <Route path="generate-image" element={<GenerateImage />} />
+        <Route path="remove-background" element={<RemoveBackground />} />
+        <Route path="remove-object" element={<RemoveObject />} />
+        <Route path="review-resume" element={<ReviewResume />} />
+        <Route path="generate-images" element={<GenerateImage />} />
+        <Route path="community" element={<Community />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/pages/ReviewResume.jsx
+++ client/src/pages/ReviewResume.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { FileText, Sparkle } from "lucide-react";
+import { useState } from "react";
 
 const ReviewResume = () => {
+  const [input, setInput] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>ReviewResume</div>
-  )
-}
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      {/*left col */}
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkle className="w-6 text-[#00DA83]" />
+          <h1 className="text-xl font-semibold">Review Resume</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload Resume</p>
+        <input
+          onChange={(e) => setInput(e.target.files[0])}
+          accept="application/pdf"
+          type="file"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          required
+        />
+        <p className="mt-2 text-xs text-gray-500">Supports PDF format only</p>
+        <p className="mt-6 text-sm font-medium">Job Description (Optional)</p>
+        <textarea
+          onChange={(e) => setJobDescription(e.target.value)}
+          value={jobDescription}
+          rows={4}
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          placeholder="Paste the job description here to get tailored feedback..."
+        />
 
-export default ReviewResume
+        <button
+          className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6
+        text-sm rounded-lg cursor-pointer"
+        >
+          <FileText className="w-5" />
+          Review Resume
+        </button>
+      </form>
+      {/*right col */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96">
+        <div className="flex items-center gap-3">
+          <FileText className="w-5 h-5 text-[#00DA83]" />
+          <h1 className="text-xl font-semibold">Analysis Resume</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <FileText className="w-9 h-9" />
+            <p>Upload a resume and click "Review Resume" to get started</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewResume;
